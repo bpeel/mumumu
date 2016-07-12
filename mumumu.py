ba49ba8d80c20e53e704780e57c12ea232a8e8a9
Neil Roberts <bpeeluk@yahoo.co.uk>
2016-07-12 15:57:09 +0100

    Aldoni funkcion por krei bildon de la filmeto

--- mumumu.py
+++ mumumu.py
@@ -2,10 +2,45 @@
 
 import subprocess
 import shutil
+import cairo
+
+BILDO_LARĜO = 1920
+BILDO_ALTO = 1080
 
 ciferoj = [ '', 'unu', 'du', 'tri', 'kvar', 'kvin', 'ses',
             'sep', 'ok', 'naŭ' ]
 dekoj = [ '', 'dek', 'cent', 'mil' ]
+
+def krei_skalskemon(surfaco, larĝo, alto):
+    surfaclarĝo = surfaco.get_width()
+    surfacalto = surfaco.get_height()
+    skemo = cairo.SurfacePattern(surfaco)
+    matrico = cairo.Matrix()
+    matrico.scale(surfaclarĝo / larĝo, surfacalto / alto)
+    skemo.set_matrix(matrico)
+    return skemo
+
+def krei_fonskemon():
+    fono = cairo.ImageSurface.create_from_png("fono.png")
+    return krei_skalskemon(fono, BILDO_LARĜO, BILDO_ALTO)
+
+fonskemo = krei_fonskemon()
+
+def krei_bovinsurfacon():
+    el_surfaco = cairo.ImageSurface.create_from_png("bovino.png")
+    alto = BILDO_ALTO // 8
+    larĝo = (alto * el_surfaco.get_width() //
+             el_surfaco.get_height())
+    skemo = krei_skalskemon(el_surfaco, larĝo, alto)
+    al_surfaco = cairo.ImageSurface(cairo.FORMAT_ARGB32, larĝo, alto)
+    cr = cairo.Context(al_surfaco)
+    cr.set_source(skemo)
+    cr.set_operator(cairo.OPERATOR_SOURCE)
+    cr.paint()
+
+    return al_surfaco
+
+bovinsurfaco = krei_bovinsurfacon()
 
 def nombro_al_teksto(nombro):
     partoj = []
@@ -27,6 +62,14 @@ def nombro_al_teksto(nombro):
 
     return " ".join(partoj)
 
+def nombro_al_frazo(nombro):
+    if nombro == 1:
+        alia_parto = "bovino"
+    else:
+        alia_parto = "bovinoj"
+
+    return nombro_al_teksto(nombro) + " " + alia_parto + " muĝas."
+
 def rulu(*argoj):
     return subprocess.check_call(argoj)
 
@@ -38,6 +81,53 @@ def sonnombro_al_dosiero(sonnombro):
 
 def muo_al_dosiero(muo):
     return "muo{}.wav".format(muo)
+
+def krei_bildon_por_nombro(nombro, muoj):
+    surfaco = cairo.ImageSurface(cairo.FORMAT_ARGB32,
+                                 BILDO_LARĜO,
+                                 BILDO_ALTO)
+    cr = cairo.Context(surfaco)
+    cr.save()
+    cr.set_source(fonskemo)
+    cr.set_operator(cairo.OPERATOR_SOURCE)
+    cr.paint()
+    cr.restore()
+
+    frazo = nombro_al_frazo(nombro)
+
+    cr.set_font_size(BILDO_ALTO / 10)
+
+    dimensioj = cr.font_extents()
+    alto_de_tiparo = dimensioj[2]
+
+    cr.save()
+    cr.move_to(BILDO_LARĜO / 100, alto_de_tiparo)
+    cr.set_source_rgb(1.0, 1.0, 1.0)
+    cr.set_line_width(8)
+    cr.text_path(frazo)
+    cr.stroke_preserve()
+    pat = cairo.LinearGradient(0, 0, 0, alto_de_tiparo)
+    pat.add_color_stop_rgb(0, 0, 198 / 255, 0)
+    pat.add_color_stop_rgb(1, 0, 76 / 255, 0)
+    cr.set_source(pat)
+    cr.fill()
+    cr.restore()
+
+    bovino_larĝo = bovinsurfaco.get_width()
+    bovino_alto = bovinsurfaco.get_height()
+
+    x = 0
+    y = alto_de_tiparo * 1.2
+    for muo in range(muoj):
+        cr.set_source_surface(bovinsurfaco, x, y)
+        cr.paint()
+        
+        x += bovino_larĝo
+        if x + bovino_larĝo > BILDO_LARĜO:
+            x = 0
+            y += bovino_alto
+
+    surfaco.write_to_png("bildo.png")
 
 muoj = [ "mu?", "mu!" ]
 
@@ -54,7 +144,7 @@ for nombro in range(1, 100):
          "-veo",
          "-w" + sonnombro_al_dosiero(sonnombro),
          "--",
-         nombro_al_teksto(nombro) + "bovinoj muĝas.")
+         nombro_al_frazo(nombro))
     sonnombro += 1
 
     for muo in range(nombro):
